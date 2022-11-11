edaf79de4aa32728adf11d3c2ad63bec7f4ee9d7
Maxim Khromov <max.khromov.ig@gmail.com>
2022-11-11 10:08:43 +0100

    home pages app starter pack

--- resources/js/app/App.jsx
+++ resources/js/app/App.jsx
@@ -1,5 +1,18 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Layout from "./Pages/Layout"
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 export default function App() {
-    return <div>Hello World</div>;
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route path ='/' element={<Home/>}/>
+                    <Route path ='/login' element={<Login/>}/>
+               </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
