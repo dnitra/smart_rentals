7b69ebdbda2fee279962b99e39b6a61aca06cd99
dnitra <109307365+dnitra@users.noreply.github.com>
2022-11-25 02:12:49 +0100

    fixed email blades

--- resources/views/mails/application.blade.php
+++ resources/views/mails/application.blade.php
@@ -1,7 +1,7 @@
 <div>
     Congratulations,
     <br>
-    <br> 
+    <br>
     You have new applicant on this address:
     <br>
     <strong>{{ $street_and_number }}</strong>
@@ -11,19 +11,16 @@
     <br>
     <h2>Applicant:</h2>
     <ul>
-       <li>Name: <strong>{{$name}}</strong></li> 
-       <li>Email: <strong>{{$email}}</strong></li> 
-       <li>Phone: <strong>{{$phone}}</strong></li> 
-       <li>Message: <strong>{{$message}}</strong></li> 
+        <li>Name: <strong>{{ $name }}</strong></li>
+        <li>Email: <strong>{{ $email }}</strong></li>
+        <li>Phone: <strong>{{ $phone }}</strong></li>
+        <li>Message: <strong>{{ $message }}</strong></li>
     </ul>
     <br>
     For more details visit your property: <a href="{{ $link }}">{{ $link }}</a>
     <br>
     <br>
-    Sincerly,
+    Sincerly Yours,
     <br>
-    Your Smart-rentals Team
-
-
-
+    Smart-rentals Team
 </div>
