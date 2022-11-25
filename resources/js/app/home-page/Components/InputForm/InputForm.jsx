27ddb185bea89c638035c13459e490e19597b92d
dnitra <109307365+dnitra@users.noreply.github.com>
2022-11-25 13:29:08 +0100

    added displaying errors when registering

--- resources/js/app/home-page/Components/InputForm/InputForm.jsx
+++ resources/js/app/home-page/Components/InputForm/InputForm.jsx
@@ -7,17 +7,24 @@ function InputForm({
     name,
     value,
     handleChange,
+    errors
 }) {
     return (
-        <input
-            className={className}
-            type={type}
-            placeholder={placeholder}
-            name={name}
-            value={value}
-            onChange={handleChange}
-            required
-        />
+        <>
+            <input
+                className={className}
+                type={type}
+                placeholder={placeholder}
+                name={name}
+                value={value}
+                onChange={handleChange}
+                required
+            />
+            {errors ? errors.map(error => {
+                return (<div className="errors">{error}</div>)
+            }): ""}
+        </>
+        
     );
 }
 
